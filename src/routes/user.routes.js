9ea3aafd162357ad17e1063f34d6bb94eb2bc787
gabriel <gabrielscotio@gmail.com>
2023-12-10 17:30:16 -0300

    middlewares and errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,15 @@ const { Router } = require("express")
 const UsersController = require("../controllers/UsersController.js")
 const userRoutes = Router()
 
+function myMiddleware(request, response, next) {
+    if(!request.body.isAdmin) {
+        return response.json({ message: "user unauthorized"})
+    }
+    next()
+}
+
 const userController = new UsersController()
 
-userRoutes.post("/", userController.create)
+userRoutes.post("/",myMiddleware, userController.create)
 
 module.exports = userRoutes
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("express-async-errors")
+const AppError = require("./utils/AppError.js")
 const express = require('express')
 const routes = require("./routes")
 const app = express()
@@ -5,5 +7,19 @@ const PORT = 3333
 
 app.use(express.json())
 app.use(routes)
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode.json({
+            status: "error",
+            message: "error.message"
+        }))
+    }
+    console.log(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "internal server error"
+    })
+})
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))

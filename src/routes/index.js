e31a0bd089360be45f0098d5efbac5fed1a29b4d
gabriel <gabrielscotio@gmail.com>
2023-12-17 18:38:52 -0300

    notes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express")
+
 const userRoutes = require("./user.routes.js")
+const notesRoutes = require("./notes.routes.js")
 const routes = Router()
 
 routes.use("/users", userRoutes)
+routes.use("/notes", notesRoutes)
 
 module.exports = routes

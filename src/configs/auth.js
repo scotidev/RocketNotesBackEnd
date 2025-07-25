eb8c7fe5221d010af38f9cc666da4b3b61b6a5aa
gabrielscoti42 <gabrielscotio@gmail.com>
2025-07-25 18:00:57 -0300

    authentication

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: "default",
+    secret: process.env.JWT_SECRET || "default_secret",
     expiresIn: "1d",
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import "express-async-errors";
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 import { AppError } from "./utils/AppError.js";
 import routes from "./routes/index.js";
@@ -7,6 +10,7 @@ import routes from "./routes/index.js";
 const app = express();
 const PORT = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
